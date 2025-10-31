4e9711694a6d669b6470a5d6486fa7fad82c1979
İbrahim Çetin <cetinibrahim.ci@gmail.com>
2025-10-31 09:37:32 +0300

    Refactor BodyBuilder to enforce single Encodable body.

--- Sources/RequestSpec/DSL/Builders/BodyBuilder.swift
+++ Sources/RequestSpec/DSL/Builders/BodyBuilder.swift
@@ -8,8 +8,8 @@
 /// Result builder for constructing request body
 @resultBuilder
 public enum BodyBuilder {
-    public static func buildBlock(_ components: (any Encodable)?) -> (any Encodable)? {
-        components
+    public static func buildBlock(_ component: (any Encodable)?) -> (any Encodable)? {
+        component
     }
 
     public static func buildExpression(_ expression: (any Encodable)?) -> (any Encodable)? {
@@ -22,6 +22,12 @@ public enum BodyBuilder {
 
     public static func buildEither(second component: (any Encodable)?) -> (any Encodable)? {
         component
+    }
+
+    /// Only one Encodable body is allowed; do not combine multiple body values.
+    @available(*, unavailable, message: "Only one Encodable body is allowed in a request.")
+    public static func buildBlock(_ components: (any Encodable)?...) -> (any Encodable)? {
+        fatalError()
     }
 
     //    public static func buildBlock(_ components: [any KeyValuePair & Encodable]...) -> any Encodable {
